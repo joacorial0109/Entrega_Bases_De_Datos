445e89a656e9664aee6b32aba2a0980849d8e45f
Alfonso <alfonso.gesto@correo.ucu.edu.uy>
2025-11-21 09:32:32 -0300

    main actualizado con consultas

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from app.rutas.sala_routes import sala_routes
 from app.rutas.participantes_routes import participantes_routes
 from app.rutas.program_routes import program_routes
 from app.rutas.sancion_routes import sancion_routes
+from app.rutas.consultas_routes import consultas_routes
 
 app = Flask(__name__)
 
@@ -15,6 +16,7 @@ app.register_blueprint(sala_routes)
 app.register_blueprint(participantes_routes)
 app.register_blueprint(program_routes)
 app.register_blueprint(sancion_routes)
+app.register_blueprint(consultas_routes)
 
 if __name__ == "__main__":
     app.run(debug=True)
